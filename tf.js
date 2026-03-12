6cd0b7b87f7d60f0366eb62905b5df0f52896479
nageswariperam21 <nageswariperam25@gmail.com>
2026-03-12 11:54:57 +0530

    Add files via upload

---
+++ tf.js
@@ -0,0 +1,17 @@
+let tfcolor="red"
+switch(tfcolor){
+case "red":{
+    console.log("stop")
+    break
+}
+case "orange":{
+console.log("ready")
+break
+}
+case "green":{
+    console.log("go")
+    break
+}
+default:
+    console.log("invalid")
+}
---
+++ week.js
@@ -0,0 +1,28 @@
+let wk="day1"
+switch(wk){
+    case "day1":{
+        console.log("monday")
+        break
+    }
+    case "day2":{
+        console.log("tuesday")
+        break
+    }
+    case "day3":{
+        console.log("wednesday")
+        break
+    }
+    case "day4":{
+        console.log("thursday")
+        break
+    }
+    case "day5":{
+        console.log("friday")
+        break
+    }
+    case "day6":{
+        console.log("saturday")
+        break
+    }
+    default:console.log("sunday")
+}
