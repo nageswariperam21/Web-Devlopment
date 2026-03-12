d87b06f0e730b24cc5b7c5248f41ea48b6d7ce73
nageswariperam21 <nageswariperam25@gmail.com>
2026-03-12 12:18:00 +0530

    Add files via upload

---
+++ op.js
@@ -0,0 +1,36 @@
+let op="+"
+switch(op){
+    case "+":{
+        console.log("add")
+        break
+    }
+    case "-":{
+        console.log("sub")
+        break
+    }
+    case "*":{
+        console.log("mul")
+        break
+    }
+    case "%":{
+        console.log("perc")
+        break
+    }
+    case "=":{
+        console.log("equal")
+        break
+    }
+    case "@":{
+        console.log("attherate")
+        break
+    }
+    case "&":{
+        console.log("and")
+        break
+    }
+    case "!":{
+        console.log("not equal")
+        break
+    }
+    default:console.log("invalid")
+}
